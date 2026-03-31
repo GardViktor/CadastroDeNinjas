ffcc39e4fced4cb33ba5d23602a3d22d6ce6b843
Edgard <edgardvictorlol@gmail.com>
2026-03-31 11:47:42 -0300

    fix: add @JsonIgnore para previnir o erro de serialização

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -22,6 +23,7 @@ public class MissoesModel {
     private char dificuldade;
     //@OneToMany uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore //Parar o Loop
     private List<NinjaModel> ninjas;
 
 }
